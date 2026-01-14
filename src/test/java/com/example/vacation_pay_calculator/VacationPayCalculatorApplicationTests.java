f304bb028aed80dd681cdb8759524d358f620006
pathfind1103 <pathfind1103@gmail.com>
2026-01-14 19:50:26 +0300

    feat: initial commit

---
+++ src/test/java/com/example/vacation_pay_calculator/VacationPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacation_pay_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
