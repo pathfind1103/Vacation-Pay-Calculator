7ea07d99b5c75c34ad5cb9ddf8d41a62bf84b33f
pathfind1103 <pathfind1103@gmail.com>
2026-01-22 20:58:40 +0300

    feat(dto): add optional startDate and endDate with date validation

--- src/main/java/com/example/vacation_pay_calculator/dto/CalculateVacationPayRequest.java
+++ src/main/java/com/example/vacation_pay_calculator/dto/CalculateVacationPayRequest.java
@@ -1,17 +1,45 @@
 package com.example.vacation_pay_calculator.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
+import java.time.LocalDate;
 
+@Builder
 @Value
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class CalculateVacationPayRequest {
 
     @Positive(message = "The average salary should be a positive number")
     double averageSalary;
 
     @Min(value = 1, message = "The number of vacation days must be at least 1")
-    int vacationDays;
+    Integer vacationDays;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    LocalDate startDate;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    LocalDate endDate;
+
+    @AssertTrue(message = "The end date must be later than or equal to the start date")
+    private boolean isDatesValid() {
+        if (startDate == null && endDate == null) {
+            return true;
+        }
+
+        if (startDate == null || endDate == null) {
+            return false;
+        }
+
+        return !endDate.isBefore(startDate);
+    }
 
 }
