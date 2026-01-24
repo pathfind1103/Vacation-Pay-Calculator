713c0a4e4a238ae1e2e8db1bc35412ed7d3dae63
pathfind1103 <pathfind1103@gmail.com>
2026-01-24 22:01:16 +0300

    refactor(controller): remove unused imports

--- src/main/java/com/example/vacation_pay_calculator/controller/VacationPayCalculatorController.java
+++ src/main/java/com/example/vacation_pay_calculator/controller/VacationPayCalculatorController.java
@@ -3,16 +3,12 @@ package com.example.vacation_pay_calculator.controller;
 import com.example.vacation_pay_calculator.dto.CalculateVacationPayRequest;
 import com.example.vacation_pay_calculator.dto.CalculateVacationPayResponse;
 import com.example.vacation_pay_calculator.service.VacationPayCalculatorService;
-import lombok.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Positive;
 
 @RestController
 public class VacationPayCalculatorController {
